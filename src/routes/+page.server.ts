db305e7fff872e4eb01386931d85dff01928b0b9
Robert Møller <rober@hjortsholm.com>
2022-09-11 20:20:06 +0200

    - Deleted API route - Stripped out uneccesary code from +page.server.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,17 +1,14 @@
-import {CALENDAR_ID, SCOPES, GOOGLE_API_CRED, API_KEY} from '$env/static/private';
+import {CALENDAR_ID, API_KEY} from '$env/static/private';
 
 import {auth as Auth, calendar as Calendar} from "@googleapis/calendar";
 import type {CalendarEvent} from "../types/calendar-event";
 
 /** @type {import('./$types').PageServerLoad<Promise<{ events:CalendarEvent[] ,calendar: string, error: string}>>} */
-export async function load(): Promise<{ events:CalendarEvent[] ,calendar: string, error: string}> {
+export async function load(): Promise<{ events: CalendarEvent[], error?: any }> {
     const auth = Auth.fromAPIKey(API_KEY);
     const calendar = Calendar({version: 'v3', auth});
     try {
-        const message = await auth.request({url: 'https://www.googleapis.com/calendar/v3/calendars/' + CALENDAR_ID + '/events'});
-        console.log('message',message);
-
-        console.debug('+page.server#load','Fetching calendar events from', CALENDAR_ID);
+        console.debug('+page.server#load', 'Fetching Google Calendar events from', CALENDAR_ID);
         const events = await calendar.events.list({
             calendarId: CALENDAR_ID,
             // timeMin: (new Date()).toISOString(), // From now to the future // todo: should this be from yesterday to show events that are happing now?
@@ -19,19 +16,12 @@ export async function load(): Promise<{ events:CalendarEvent[] ,calendar: string
             singleEvents: true,
             orderBy: 'startTime',
         });
-
-        console.debug('+page.server#load','Received events', events.data.items);
-
-        if (!events.data.items) return {events: [], calendar: CALENDAR_ID + ', ' + SCOPES , error: 'no items found'};
-        return {events: events.data.items.map((event: any) => event as CalendarEvent), calendar: CALENDAR_ID+', '+ SCOPES, error: ''};
+        console.debug('+page.server#load', 'Received events', events.data.items);
+        if (!events.data.items) return {events: [], error: 'No events found'};
+        return {events: events.data.items.map((event: any) => event as CalendarEvent)};
     } catch (err) {
-        console.error('+page.server#load','Google Calendar returned an error: '+err);
-        console.error(err)
-        return {events: [], calendar: CALENDAR_ID + ', ' + SCOPES, error: 'Google Calendar returned an error: ' + err}; // todo: +error.svelte & throw error(500,'fubar')
+        console.error('+page.server#load', 'Google Calendar returned an error: ' + err);
+        console.error(erra)
+        return {events: [], error: err};
     }
-
-
-
-    return {events: [], calendar: CALENDAR_ID + ', ' + SCOPES, error: ''};
-
 }
