f091ccb877d1b95583ea35d5cd7232c03ee1b886
Robert Møller <rober@hjortsholm.com>
2022-09-11 14:24:22 +0200

    Revert "Removed timezone string from calendar dates"
    This reverts commit 47f9b194c118c646541d1f88cddcb1d12febb5e0.

--- src/types/calendar-event.ts
+++ src/types/calendar-event.ts
@@ -5,16 +5,16 @@ export type CalendarEvent = {
     readonly status: string;
     readonly htmlLink: string;
     readonly created: string;
-    readonly updated?: string;
+    readonly updated: string;
     readonly summary: string;
     readonly description?: string;
     readonly location?: string;
     readonly creator: { email: string, self: boolean };
     readonly organizer: { email: string, self: boolean };
-    readonly start: { dateTime: string };
-    readonly end?: { dateTime: string };
-    readonly iCalUID?: string;
-    readonly sequence?: number;
-    readonly reminders?: { useDefault: boolean };
-    readonly eventType?: string;
+    readonly start: { dateTime: string, timeZone: string };
+    readonly end?: { dateTime: string, timeZone: string };
+    readonly iCalUID: string;
+    readonly sequence: number;
+    readonly reminders: { useDefault: boolean };
+    readonly eventType: string;
 }
