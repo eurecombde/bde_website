368d33bf9e1c3c5239b44f92da22b5586797dc6e
Robert Møller <rober@hjortsholm.com>
2022-09-11 14:56:00 +0200

    Exploring env variables

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -19,10 +19,10 @@ export async function load(): Promise<{ events:CalendarEvent[] ,calendar: string
 
         console.debug('Received events', events.data.items);
 
-        if (!events.data.items) return {events: [], calendar: CALENDAR_ID, error: 'no items found'};
-        return {events: events.data.items.map((event: any) => event as CalendarEvent), calendar: CALENDAR_ID, error: ''};
+        if (!events.data.items) return {events: [], calendar: CALENDAR_ID + ', ' + SCOPES + ', ' + SERVICE_ACCOUNT_EMAIL + ', ' + SERVICE_ACCOUNT_PRIVATE_KEY, error: 'no items found'};
+        return {events: events.data.items.map((event: any) => event as CalendarEvent), calendar: CALENDAR_ID+', '+ SCOPES+', '+ SERVICE_ACCOUNT_EMAIL+', '+ SERVICE_ACCOUNT_PRIVATE_KEY, error: ''};
     } catch (err) {
         console.error('Google Calendar returned an error: ' + err);
-        return {events: [], calendar: CALENDAR_ID, error: 'Google Calendar returned an error: ' + err}; // todo: +error.svelte & throw error(500,'fubar')
+        return {events: [], calendar: CALENDAR_ID + ', ' + SCOPES + ', ' + SERVICE_ACCOUNT_EMAIL + ', ' + SERVICE_ACCOUNT_PRIVATE_KEY, error: 'Google Calendar returned an error: ' + err}; // todo: +error.svelte & throw error(500,'fubar')
     }
 }
