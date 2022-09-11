9b8df9e580152cdb97996d666ff3e4c8f3628272
Robert Møller <rober@hjortsholm.com>
2022-09-11 12:33:40 +0200

    Added Netlify edge deployment config

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,22 @@
-import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: [
-		preprocess({
-			postcss: true,
-		}),
-	],
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: [
+        preprocess({
+            postcss: true,
+        }),
+    ],
 
-	kit: {
-		adapter: adapter()
-	}
+    kit: {
+        adapter: adapter({
+            edge: true,
+            split: false
+        })
+    }
 };
 
 export default config;
