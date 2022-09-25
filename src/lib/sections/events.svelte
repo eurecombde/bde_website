7cd4187cf0cd2950ab785fa191abff06931dbae7
Robert Møller <rober@hjortsholm.com>
2022-09-25 15:27:02 +0200

    - Changed toast copy

--- src/lib/sections/events.svelte
+++ src/lib/sections/events.svelte
@@ -10,8 +10,8 @@
         addToast({type: ToastType.Success, message: 'iCal link copied to clipboard'});
     }
 
-    function downloadSuccessfully() {
-        addToast({type: ToastType.Success, message: 'iCal file downloaded'});
+    function downloadStarted() {
+        addToast({type: ToastType.Success, message: 'Download started'});
     }
 </script>
 
@@ -27,7 +27,7 @@
                             Download our calendar to follow all the <span class="underline decoration-blue-500">current events</span> events we have planned. Alternatively, you can subscribe to stay up to date with <span class="underline decoration-blue-500">future events</span> as well, using your favourite calendar app.
                         </p>
                         <div class="flex">
-                            <a href={ical} on:click={downloadSuccessfully} class="shrink ml-auto mr-2 bg-transparent ml-auto text-black-300 hover:text-white text-center rounded shadow hover:shadow-lg py-2 px-4 border border-black-300 hover:border-black hover:bg-black">
+                            <a href={ical} on:click={downloadStarted} class="shrink ml-auto mr-2 bg-transparent ml-auto text-black-300 hover:text-white text-center rounded shadow hover:shadow-lg py-2 px-4 border border-black-300 hover:border-black hover:bg-black">
                                 Current events
                             </a>
 
