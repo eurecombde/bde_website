a05b11117a1c174693055e5ea78553fa5372e226
Robert Møller <rober@hjortsholm.com>
2022-10-08 21:10:47 +0200

    Enabled contact us form

--- src/lib/sections/contact.svelte
+++ src/lib/sections/contact.svelte
@@ -52,41 +52,41 @@
                 </div>
             </div>
 
-<!--            <div class="mt-8 lg:w-1/2 lg:mx-6">-->
-<!--                <div-->
-<!--                        class="w-full px-8 py-10 mx-auto overflow-hidden bg-white rounded-lg shadow-2xl dark:bg-gray-900 lg:max-w-xl shadow-gray-300/50 dark:shadow-black/50">-->
-<!--                    <h1 class="text-lg font-medium text-gray-700">What do you want to ask</h1>-->
+            <div class="mt-8 lg:w-1/2 lg:mx-6">
+                <div
+                        class="w-full px-8 py-10 mx-auto overflow-hidden bg-white rounded-lg shadow-2xl dark:bg-gray-900 lg:max-w-xl shadow-gray-300/50 dark:shadow-black/50">
+                    <h1 class="text-lg font-medium text-gray-700">What do you want to ask</h1>
 
-<!--                    <form class="mt-6" name="contact" netlify>-->
-<!--                    &lt;!&ndash; action="/about#contact?success">&ndash;&gt;-->
-<!--                        <div class="flex-1">-->
-<!--                            <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Full Name</label>-->
-<!--                            <input required type="text" placeholder="John Doe" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"/>-->
-<!--                        </div>-->
+                    <form class="mt-6" name="contact" netlify>
+                    <!-- action="/about#contact?success">-->
+                        <div class="flex-1">
+                            <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Full Name</label>
+                            <input required type="text" placeholder="John Doe" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"/>
+                        </div>
 
-<!--                        <div class="flex-1 mt-6">-->
-<!--                            <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email address</label>-->
-<!--                            <input required type="email" placeholder="johndoe@example.com"-->
-<!--                                   class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"/>-->
-<!--                        </div>-->
+                        <div class="flex-1 mt-6">
+                            <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email address</label>
+                            <input required type="email" placeholder="johndoe@example.com"
+                                   class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"/>
+                        </div>
 
-<!--                        <div class="w-full mt-6">-->
-<!--                            <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Message</label>-->
-<!--                            <textarea required class="block w-full h-32 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md md:h-48 dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"-->
-<!--                                      placeholder="Message"></textarea>-->
-<!--                        </div>-->
+                        <div class="w-full mt-6">
+                            <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Message</label>
+                            <textarea required class="block w-full h-32 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md md:h-48 dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                                      placeholder="Message"></textarea>
+                        </div>
 
-<!--                        <button type="submit" class="cursor-pointer  w-full px-6 py-3 mt-6 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">-->
-<!--                            get in touch-->
-<!--                        </button>-->
-<!--                        &lt;!&ndash;{#if success}&ndash;&gt;-->
-<!--                        &lt;!&ndash;    <div>&ndash;&gt;-->
-<!--                        &lt;!&ndash;        Message sent successfully&ndash;&gt;-->
-<!--                        &lt;!&ndash;    </div>&ndash;&gt;-->
-<!--                        &lt;!&ndash;{/if}&ndash;&gt;-->
-<!--                    </form>-->
-<!--                </div>-->
-<!--            </div>-->
+                        <button type="submit" class="cursor-pointer  w-full px-6 py-3 mt-6 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                            get in touch
+                        </button>
+                        <!--{#if success}-->
+                        <!--    <div>-->
+                        <!--        Message sent successfully-->
+                        <!--    </div>-->
+                        <!--{/if}-->
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 </section>
