7e8ba87c24ebe35f7d65279e275bb57332038972
Robert Møller <rober@hjortsholm.com>
2022-10-09 17:50:06 +0200

    Added list of guides

--- src/lib/constants/student-guides.ts
+++ src/lib/constants/student-guides.ts
@@ -12,14 +12,28 @@ export const guides: Guide[] = [
     {
         name: 'Bars and Clubs',
         slug: 'bars-and-clubs',
-        description: 'A guide to the best bars and clubs in the area.',
+        description: 'The best bars and clubs in the area.',
         // image: 'https://images.unsplash.com/photo-1517436073-3b8b4f3b5f9c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
-        author: 'John Doe',
+        author: 'TBC',
         date: '2020-01-01',
         tags: ['bars', 'clubs', 'nightlife'],
         content: `
         Dette kan bli bra
         eller hva tenker du?
+        `
+    },
+
+    {
+        name: 'Getting to around the area',
+        slug: 'transport',
+        description: 'How to get around the area.',
+        // image: 'https://images.unsplash.com/photo-1517436073-3b8b4f3b5f9c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        author: 'TBC',
+        date: '2022-01-01',
+        tags: ['transport', 'buses', 'trains'],
+        content: `
+        How to get around Antibes and the surrounding area by public transport?
+         
         `
     }
 ];
--- src/routes/student-life/+page.svelte
+++ src/routes/student-life/+page.svelte
@@ -19,11 +19,11 @@
 
         <!--Headline-->
         <div class="mt-8 xl:mt-16 lg:flex lg:-mx-6 lg:flex">
-            <div class="lg:w-3/4 lg:px-6">
+            <a class="lg:w-3/4 lg:px-6" href="/student-life/{activeGuide.slug}">
                 {#if activeGuide.image}
-                    <img class="object-cover object-center w-full h-80 xl:h-[28rem] rounded-xl" src={activeGuide.image} alt="">
+                    <img class="object-cover object-center w-full h-80 xl:h-[28rem] rounded-xl" src={activeGuide.image} alt={activeGuide.tags.join(', ')}>
                 {:else}
-                    <img class="object-cover object-center w-full h-80 xl:h-[28rem] rounded-xl" src="{activeGuide.image}" alt="">
+                    <img class="object-cover object-center w-full h-80 xl:h-[28rem] rounded-xl" src="https://source.unsplash.com/1080x720/?{activeGuide.tags.join(',')}" alt={activeGuide.tags.join(', ')}>
                 {/if}
                 <div>
                     <p class="mt-6 text-sm text-blue-500 uppercase">{activeGuide.tags.join(", ")}</p>
@@ -32,25 +32,22 @@
                         {activeGuide.name}
                     </h1>
 
-                    <div class="flex items-center mt-6">
-                        <div>
-                            <h1 class="text-sm text-gray-700 dark:text-gray-200">Amelia. Anderson</h1>
-                            <p class="text-sm text-gray-500 dark:text-gray-400">Lead Developer</p>
-                        </div>
-                    </div>
+                    <h1 class="text-sm text-gray-700 dark:text-gray-200">By {activeGuide.author}</h1>
                 </div>
-            </div>
+            </a>
 
             <!--Guide list-->
             <div class="mt-8 lg:w-1/4 lg:mt-0 lg:px-6">
                 {#each otherGuides as guide}
                     <div>
-                        <h3 class="text-blue-500">{guide.tags.join(", ")}</h3>
-
+                        <span class="text-blue-500 uppercase text-sm">{guide.tags.join(", ")}</span>
                         <a href="/student-life/{guide.slug}" class="block mt-2 font-medium text-gray-700 hover:underline hover:text-gray-500 dark:text-gray-400 ">
                             {guide.name}
                         </a>
+                        <h1 class="mt-2 text-sm text-gray-700 dark:text-gray-200">By {activeGuide.author}</h1>
                     </div>
+
+
 
                     <hr class="my-6 border-gray-200 dark:border-gray-700">
                 {/each}
