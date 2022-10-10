ed6ea7fb796b1a96015859d8a977f41d411041ec
Robert Moller <33206528+roberthmoller@users.noreply.github.com>
2022-10-10 07:36:24 +0000

    Added more clubs

--- src/lib/constants/clubs.ts
+++ src/lib/constants/clubs.ts
@@ -4,9 +4,20 @@ export type Club = { name: string, category: Category, groupLink?: string, photo
 const SPORT: Category = {name: "Sport", emoji: "🧗‍"};
 const SOCIAL: Category = {name: "Social", emoji: "🍷"};
 const ENTERTAINMENT: Category = {name: "Entertainment", emoji: "🎮"};
+const EDUCATIONAL: Category = {name: "Educational", emoji: "👨‍💻"};
+
 export const clubs: Club[] = [
     {name: "Climbing", category: SPORT, featured: true},
     {name: "Basketball", category: SPORT, featured: true},
-    {name: "Wine tasting", category: SOCIAL, featured: true},
+    {name: "Wine & Beer", category: SOCIAL, featured: true},
     {name: "Board Games", category: SOCIAL, featured: false},
+    {name: "Skiing", category: SPORT, featured: false},
+    {name: "Football", category: SPORT, featured: true},
+    {name: "Sunset (Art)", category: SOCIAL, featured: false},
+    {name: "International Food", category: SOCIAL, featured: true},
+    {name: "DJing", category: ENTERTAINMENT, featured: true},
+    {name: "Beach Volley", category: SPORT, featured: false},
+    {name: "Scuba Diving/Snorkling", category: SPORT, featured: false},
+    {name: "SAP", category: EDUCATIONAL, featured: false},
+    {name: "Sailing", category: SPORT, featured: false},
 ];
