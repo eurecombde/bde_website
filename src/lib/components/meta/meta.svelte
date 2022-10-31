a011b448b1ced5204d350691e9d4ed4b84d363ec
Robert Møller <rober@hjortsholm.com>
2022-10-31 14:50:48 +0100

    Fixed typo in metadata

--- src/lib/components/meta/meta.svelte
+++ src/lib/components/meta/meta.svelte
@@ -8,17 +8,17 @@
   <meta property="og:locale" content={meta.locale} />
   <meta property="og:type" content={meta.type} />
   <meta property="og:title" content={meta.title} />
-  <meta property="og:desctiption" content={meta.description} />
+  <meta property="og:description" content={meta.description} />
   <meta property="og:url" content={meta.url} />
   <meta property="og:image" content={meta.image.url} />
   <meta property="og:image:secure_url" content={meta.image.url} />
   <meta property="og:image:alt" content={meta.image.alt} />
 
   <!-- Twitter Meta Tags -->
-  <!-- <meta name="twitter:card" content={meta.image.url} />
+  <meta name="twitter:card" content={meta.image.url} />
   <meta property="twitter:domain" content={meta.domain} />
   <meta property="twitter:url" content={meta.url} />
   <meta name="twitter:title" content="EURECOM BDE" />
   <meta name="twitter:description" content={meta.description} />
-  <meta name="twitter:image" content={meta.image.url} />  -->
+  <meta name="twitter:image" content={meta.image.url} /> 
 </svelte:head>
